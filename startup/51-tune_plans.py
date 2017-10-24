9667f9f58f6f614b0b982890101eccd746798a5c
Pete Jemian <prjemian@gmail.com>
2017-10-24 18:54:19 -0500

    docs, relax EpicsMotor requirement, use COM instead of max

--- startup/51-tune_plans.py
+++ startup/51-tune_plans.py
@@ -8,15 +8,60 @@ from bluesky.utils import Msg, separate_devices
 def maximize_signal(
         detectors, signal, motor, 
         start, stop, num_points, min_step, 
+        step_factor = 2,
         *, md=None):
     """
-    tune a motor by maximizing a signal
+    plan: tune a motor by maximizing a signal
+    
+    Initially, traverse the range from start to stop with
+    the number of points specified.  Repeat with progressively
+    smaller step size until the minimum step size is reached.
+    Rescans will be centered on the signal center of mass
+    (for $I(x)$, COM$= \sum{I}/\sum{x*I}$)
+    with a scan range of 2*step_factor*step of current scan.
+
+    Parameters
+    ----------
+    detectors : (Signal) list of signals
+    signal : (str) name of signal to maximize
+    motor : (Mover) positioner to be moved
+    start : (float) start of range
+    stop : (float) end of range, note: start < stop
+    num_points : (int) number of points with each step size
+    min_step : (float) smallest step size to use.
+        note: For EpicsMotors, ``min_step=min(min_step,motor.MRES)``
+        is used.
+    step_factor : (float) used in calculating range when 
+        maximum is found, note: step_factor > 0
+
+    Example
+    -------
+    motor = Mover('motor', {'motor': lambda x: x}, {'x': 0})
+    det = SynGauss('det', m1, 'm1', center=-1.3, Imax=1e5, sigma=0.021)
+    RE(maximize_signal([det], "det", motor, -1.5, -0.5, 10, 0.01))
+
+    m1 = EpicsMotor('xxx:m1', name='m1')
+    synthetic_pseudovoigt = SynPseudoVoigt(
+            'synthetic_pseudovoigt', m1, 'm1',
+            center=-1.5 + 0.5*np.random.uniform(),
+            eta=0.3 + 0.5*np.random.uniform(),
+            sigma=0.001 + 0.05*np.random.uniform(),
+            scale=1e5)
+    RE(
+        maximize_signal(
+            [synthetic_pseudovoigt], "synthetic_pseudovoigt", 
+            motor, 
+            -2, 0, 10, 0.001
+        )
+    )
     """
     assert(isinstance(num_points, int))
     assert(num_points > 1)
     assert(start < stop)
-    assert(isinstance(motor, EpicsMotor))
-    min_step = max(min_step, epics.caget(motor.prefix+".MRES"))
+    assert(step_factor > 0)
+    # assert(isinstance(motor, EpicsMotor))
+    if isinstance(motor, EpicsMotor):
+        min_step = max(min_step, epics.caget(motor.prefix+".MRES"))
     _md = {'detectors': [det.name for det in detectors],
            'motors': [motor.name],
            'plan_args': {'detectors': list(map(repr, detectors)),
@@ -41,10 +86,11 @@ def maximize_signal(
     def _tune_core(start, stop, num_points, signal):
         next_pos = start
         step = (stop - start) / (num_points - 1)
-        max_I = None
         peak_position = None
         cur_I = None
         cur_det = {}
+        sum_I = 0		# for peak center-of-mass calculation, I(x)
+        sum_xI = 0
         
         while step >= min_step:
             yield Msg('checkpoint')
@@ -57,28 +103,33 @@ def maximize_signal(
                 cur_det = yield Msg('read', det)
                 if signal in cur_det:
                     cur_I = cur_det[signal]['value']
-                    if max_I is None or cur_I > max_I:
-                        motor.get()
-                        max_I = cur_I
-                        peak_position = motor.user_readback.value
+                    sum_I += cur_I
+                    position = motor.read()[motor.name]["value"]
+                    sum_xI += position * cur_I
 
             yield Msg('save')
 
             if next_pos < stop: # FIXME: "<" assumes sign of scan direction
                 next_pos += step
             else:
-                # TODO: use COM instead
+                if sum_I == 0:
+                    return
+                peak_position = sum_xI / sum_I  # COM
                 # TODO: report current peak_position in metadata
-                start = peak_position - 2*step
-                stop = peak_position + 2*step
+                RE.md["peak_position"] = str(peak_position)
+                start = peak_position - step_factor*step
+                stop = peak_position + step_factor*step
                 step = (stop - start) / (num_points - 1)
                 next_pos = start
 
         # finally, move to peak position
         if peak_position is not None:
+            RE.md["peak_position"] = peak_position
             yield from bp.mv(motor, peak_position)
 
-    return (yield from _tune_core(start, stop, num_points, signal))
+    yield from _tune_core(start, stop, num_points, signal)
+    print("Peak at ", RE.md["peak_position"])
+    return
 
 
 if False:       # demo & testing code
@@ -87,13 +138,13 @@ if False:       # demo & testing code
     
     RE(
         maximize_signal(
-            [syntheic_pseudovoigt], "syntheic_pseudovoigt", m1, 
+            [synthetic_pseudovoigt], "synthetic_pseudovoigt", m1, 
             -2, 0, 10, 0.00001
         )
     )
     RE(
         bp.adaptive_scan(
-            [syntheic_pseudovoigt], 'syntheic_pseudovoigt', m1, 
+            [synthetic_pseudovoigt], 'synthetic_pseudovoigt', m1, 
             start=-2, stop=0, min_step=0.01, max_step=1, 
             target_delta=500, backstep=True
         )
