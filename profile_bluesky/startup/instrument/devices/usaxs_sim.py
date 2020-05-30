30bc5ab1a0e0abcceda436ef9311c9bd370007e3
Pete R. Jemian <prjemian@gmail.com>
2020-05-30 10:26:27 -0500

    MNT ready to try on USAXS

--- profile_bluesky/startup/instrument/devices/usaxs_sim.py
+++ profile_bluesky/startup/instrument/devices/usaxs_sim.py
@@ -1,6 +1,10 @@
 
 """
 diagnose and resolve problems with USAXS tunable motors
+
+    RE(m1.tuner.tune())
+    RE(m1.tuner.tune(width=.15, num=13))
+    RE(m1.tuner.multi_pass_tune(num=6))
 """
 
 __all__ = [
@@ -13,21 +17,14 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
-from apstools.devices import AxisTunerMixin
 from apstools.devices import setup_lorentzian_swait
 from apstools.devices import SimulatedApsPssShutterWithStatus
-from apstools.plans import TuneAxis
 from apstools.synApps import UserCalcsDevice
 from bluesky import plan_stubs as bps
 import numpy as np
-from ophyd import Component, EpicsMotor, Signal
 from ophyd import EpicsSignalRO
 
-# custom for any overrides (none now)
-class UsaxsMotor(EpicsMotor): ...
-
-class UsaxsMotorTunable(AxisTunerMixin, UsaxsMotor):
-    width = Component(Signal, value=0)
+from .usaxs_sim_local import *
 
 
 shutter = SimulatedApsPssShutterWithStatus(
@@ -57,10 +54,6 @@ def hook_pre_tune_m1(axis):
 def hook_post_tune_m1(axis):
     yield from bps.mv(shutter, "close")
 
-m1.tuner = TuneAxis([noisy], m1)
+m1.tuner = UsaxsTuneAxis([noisy], m1)
 m1.pre_tune_method = hook_pre_tune_m1
 m1.post_tune_method = hook_post_tune_m1
-
-
-# RE(m1.tuner.tune())
-# RE(m1.tuner.multi_pass_tune(num=6))
