f749b515978ab3103f9972587091f1b7fe1b06eb
Pete R. Jemian <prjemian@gmail.com>
2020-08-22 07:54:55 -0500

    ... in case

--- instrument/usaxs/__init__.py
+++ instrument/usaxs/__init__.py
@@ -1,4 +1,10 @@
 
+try:
+    import ophyd
+    ophyd.EpicsSignal.set_default_timeout(timeout=10, connection_timeout=5)
+except RuntimeError:
+    pass
+
 from .motors import *
 from .scalers import *
 from .tune_guard_slits import *
